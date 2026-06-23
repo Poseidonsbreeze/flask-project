4f266f9d43e76f02142a76e206afeab17f154e68
Wealthometer <wealthhajoh87@gmail.com>
2026-06-23 18:11:42 +0100

    Update auth_routes.py

--- backend/app/routes/auth_routes.py
+++ backend/app/routes/auth_routes.py
@@ -1,7 +1,6 @@
 from flask import Blueprint, request, jsonify
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_jwt_extended import create_access_token
-from app import db
 from app.models import User
 
 auth_bp = Blueprint("auth", __name__)
